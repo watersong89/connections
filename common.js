4da3bca35b3b5c87f47c21ce15121f975823704d
Watersong89 <watersongordon@gmail.com>
2023-11-07 17:47:54 +0000

    js reorganised and input page started

---
+++ common.js
@@ -0,0 +1,26 @@
+export const groups = [
+  {
+    name: 'groupOne',
+    elements: ['a', 'b', 'c', 'd'],
+    connection: 'Letters',
+    backgroundColor: '#F9DF6D',
+  },
+  {
+    name: 'groupTwo',
+    elements: ['1', '2', '3', '4'],
+    connection: 'Numbers',
+    backgroundColor: '#9FC35A',
+  },
+  {
+    name: 'groupThree',
+    elements: ['Red', 'Blue', 'Green', 'Orange'],
+    connection: 'Colours',
+    backgroundColor: '#B1C4EF',
+  },
+  {
+    name: 'groupFour',
+    elements: ['North', 'South', 'East', 'West'],
+    connection: 'Directions',
+    backgroundColor: '#BA81C5',
+  },
+]
